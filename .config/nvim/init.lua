2ed598fcb3ec70fc40cf60a48f8410c7d41b5821
markuserik <markus13671@gmail.com>
2025-03-12 22:33:02 +0100

    Add tab key for LSP completion

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -62,3 +62,6 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- Tab for LSP confirm
+vim.cmd('inoremap <expr> <TAB> coc#pum#visible() ? coc#_select_confirm() : "<TAB>"')
